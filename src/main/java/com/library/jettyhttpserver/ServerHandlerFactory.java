46d8aee1cb701d64443525daf437ffacd50f778f
smallgod <davies.mugume@gmail.com>
2017-02-03 20:54:19 +0300

    Feb 2rd, 2016 20hr54

--- src/main/java/com/library/jettyhttpserver/ServerHandlerFactory.java
+++ src/main/java/com/library/jettyhttpserver/ServerHandlerFactory.java
@@ -6,6 +6,8 @@
 package com.library.jettyhttpserver;
 
 import com.library.utilities.LoggerUtil;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.Servlet;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
@@ -15,7 +17,9 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.FragmentConfiguration;
 import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
@@ -40,6 +44,13 @@ public class ServerHandlerFactory {
 
         WebAppContext webAppHandler = new WebAppContext();
 
+        // Add the filter, and then use the provided FilterHolder to configure it
+        FilterHolder cors = webAppHandler.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
+
         webAppHandler.setContextPath(contextPath);
 
         // webAppHandler.setWar(ApplicationPropertyLoader.getInstance().getJarFolderName() + "/src/main/recontool/war/recontool.war");
@@ -50,14 +61,15 @@ public class ServerHandlerFactory {
         webAppHandler.setDescriptor(webDescriptor);
 
         webAppHandler.setConfigurations(new Configuration[]{
-            new AnnotationConfiguration(),
-            new WebXmlConfiguration(),
+            
             new WebInfConfiguration(),
-            new PlusConfiguration(),
+            new WebXmlConfiguration(),
             new MetaInfConfiguration(),
             new FragmentConfiguration(),
-            new JettyWebXmlConfiguration(),
-            new EnvConfiguration()});
+            new EnvConfiguration(),
+            new PlusConfiguration(),
+            new AnnotationConfiguration(),
+            new JettyWebXmlConfiguration()});
 
         // webAppHandler.setDefaultDescriptor(ApplicationPropertyLoader.getInstance().getJarFolderName() + "/src/main/recontool/webdefault/webdefault.xml"); //copy from The location is JETTY_HOME/etc/webdefault.xml     
         webAppHandler.setParentLoaderPriority(true);//make the ClassLoader behavior more akin to standard Java (as opposed to the reverse requirements for Servlets)
@@ -129,25 +141,24 @@ public class ServerHandlerFactory {
         servletHandler.setContextPath(contextPath);
         servletHandler.setResourceBase(resourceBase);
         servletHandler.setWelcomeFiles(welcomeFiles);
-        
+
         //servletHandler.addServlet(new ServletHolder(new HelloServlet()), "/*");
         //servletHandler.addServlet(new ServletHolder(new HelloServlet("Buongiorno Mondo")), "/it/*");
         //servletHandler.addServlet(new ServletHolder(new HelloServlet("Bonjour le Monde")), "/fr/*");
-        
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
 
         return servletHandler;
     }
-    
+
     /**
-     * 
+     *
      * @param welcomeFiles
      * @param resourceBase
      * @param contextPath
      * @param multipartServlet
      * @param tempFolder
-     * @return 
+     * @return
      */
     protected Handler createMultipartServletContextHandler(String[] welcomeFiles, String resourceBase, String contextPath, Servlet multipartServlet, String tempFolder) {
 
@@ -155,15 +166,13 @@ public class ServerHandlerFactory {
         servletHandler.setContextPath(contextPath);
         servletHandler.setResourceBase(resourceBase);
         servletHandler.setWelcomeFiles(welcomeFiles);
-        
+
         //servletHandler.addServlet(new ServletHolder(new HelloServlet()), "/*");
         //servletHandler.addServlet(new ServletHolder(new HelloServlet("Buongiorno Mondo")), "/it/*");
         //servletHandler.addServlet(new ServletHolder(new HelloServlet("Bonjour le Monde")), "/fr/*");
-        
-
         ServletHolder fileUploadServletHolder = new ServletHolder(multipartServlet);
         fileUploadServletHolder.getRegistration().setMultipartConfig(new MultipartConfigElement(tempFolder));
-        
+
         servletHandler.addServlet(fileUploadServletHolder, "/uploadfile");
 
         return servletHandler;
